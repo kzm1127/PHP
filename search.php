2869d53438a390ed69a4919eec8de7ead1f2ff36
kzm1127 <kzm1127@gmail.com>
2017-04-24 23:29:13 +0000

    com

---
+++ search.php
@@ -0,0 +1,14 @@
+<?php
+$keyword=$_GET["keyword"];
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta charset="utf-8">
+     <title>IT CARET</title>
+   </head>
+   <body>
+     Input keyword:<?php echo $keyword; ?>
+   </body>
+ </html>
